84fa4d041e891945d622406cb9333ea00fc3264a
Mario Oliver <mar.mariooliver93@gmail.com>
2023-08-17 17:25:49 -0400

    added an info modal that appears when a user clicks to get more info that details teh movie and opens our playbutton

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { signOut } from 'next-auth/react';
+import useCurrentUser from '@/hooks/useCurrentUser';
 
 interface AccountMenuProps {
   visible?: boolean;
@@ -7,6 +8,7 @@ interface AccountMenuProps {
 
 const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
   if (!visible) return null;
+  const { data } = useCurrentUser();
   return (
     <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex">
       <div className="flex flex-col gap-3">
@@ -17,7 +19,7 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
             alt=""
           ></img>
           <p className="text-white text-sm group-hover/item:underline">
-            Username
+            {data?.name}
           </p>
         </div>
         <hr className="bg-gray-600 border-0 h-px my-4" />
