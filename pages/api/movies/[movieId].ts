63bdd9a5c9e94226ebf298b0c5726c5590e5edc5
Mario Oliver <mar.mariooliver93@gmail.com>
2023-08-17 16:36:51 -0400

    created a page and api for fetching teh movie and being able to plauy it from any of our play buttons

---
+++ pages/api/movies/[movieId].ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prismadb from '@/lib/prismadb';
+import serverAuth from '@/lib/serverAuth';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'GET') return res.status(405).end();
+
+  try {
+    await serverAuth(req, res);
+    //in nextjs when you define a route with [] you know that the query is added to the api call
+    const { movieId } = req.query;
+    if (typeof movieId !== 'string') throw new Error('Invalid Id');
+    if (!movieId) throw new Error('Invalid Id');
+    const movie = await prismadb.movie.findUnique({
+      where: {
+        id: movieId,
+      },
+    });
+    if (!movie) throw new Error('Invalid Id');
+    return res.status(200).json(movie);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
+}
